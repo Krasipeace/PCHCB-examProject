97dd35664c66a460c8fdb93a196faf86121014be
Krasimir Dramaliev <krasipeace@abv.bg>
2023-07-21 14:32:59 +0300

    Update SuccessMessages.cs

--- PCHCB/PCHCB.Common/SuccessMessages.cs
+++ PCHCB/PCHCB.Common/SuccessMessages.cs
@@ -3,5 +3,35 @@
     public static class SuccessMessages
     {
         public const string CaseAddedSuccessfully = "Case has been added successfully";
+        public const string CaseEditedSuccessfully = "Case has been edited successfully";
+        public const string CaseDeletedSuccessfully = "Case has been deleted successfully";
+
+        public const string CoolerAddedSuccessfully = "Cooler has been added successfully";
+        public const string CoolerEditedSuccessfully = "Cooler has been edited successfully";
+        public const string CoolerDeletedSuccessfully = "Cooler has been deleted successfully";
+
+        public const string CpuAddedSuccessfully = "CPU has been added successfully";
+        public const string CpuEditedSuccessfully = "CPU has been edited successfully";
+        public const string CpuDeletedSuccessfully = "CPU has been deleted successfully";
+
+        public const string GpuAddedSuccessfully = "GPU has been added successfully";
+        public const string GpuEditedSuccessfully = "GPU has been edited successfully";
+        public const string GpuDeletedSuccessfully = "GPU has been deleted successfully";
+
+        public const string MotherboardAddedSuccessfully = "Motherboard has been added successfully";
+        public const string MotherboardEditedSuccessfully = "Motherboard has been edited successfully";
+        public const string MotherboardDeletedSuccessfully = "Motherboard has been deleted successfully";
+
+        public const string PowerSupplyAddedSuccessfully = "Power Supply has been added successfully";
+        public const string PowerSupplyEditedSuccessfully = "Power Supply has been edited successfully";
+        public const string PowerSupplyDeletedSuccessfully = "Power Supply has been deleted successfully";
+
+        public const string RamAddedSuccessfully = "RAM has been added successfully";
+        public const string RamEditedSuccessfully = "RAM has been edited successfully";
+        public const string RamDeletedSuccessfully = "RAM has been deleted successfully";
+
+        public const string StorageAddedSuccessfully = "Storage has been added successfully";
+        public const string StorageEditedSuccessfully = "Storage has been edited successfully";
+        public const string StorageDeletedSuccessfully = "Storage has been deleted successfully";
     }
 }
