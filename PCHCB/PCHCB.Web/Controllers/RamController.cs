bab0f7279ec87731791953e36696483b654b31fb
Krasimir Dramaliev <krasipeace@abv.bg>
2023-07-22 22:36:49 +0300

    added ramController + viewModel + view

--- PCHCB/PCHCB.Web/Controllers/RamController.cs
+++ PCHCB/PCHCB.Web/Controllers/RamController.cs
@@ -3,12 +3,80 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
+    using PCHCB.Services.Data.Contracts;
+    using PCHCB.Web.Infrastructure.Extensions;
+    using PCHCB.Web.ViewModels.Ram;
+
+    using static PCHCB.Common.NotificationMessages;
+    using static PCHCB.Common.ErrorMessages.Provider;
+    using static PCHCB.Common.SuccessMessages;
+    using static PCHCB.Common.ExceptionMessages;
+
     [Authorize]
     public class RamController : Controller
     {
-        public IActionResult Index()
+        private readonly IRamService ramService;
+        private readonly IProviderService providerService;
+
+        public RamController(IRamService ramService, IProviderService providerService)
         {
-            return View();
+            this.ramService = ramService;
+            this.providerService = providerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            bool isProvider = await this.providerService
+                .ProviderExistsByUserIdAsync(this.User.GetId()!);
+
+            if (!isProvider)
+            {
+                this.TempData[ErrorMessage] = UserCannotAddRamsErrorMessage;
+
+                return this.RedirectToAction("BecomeProvider", "Provider");
+            }
+
+            RamFormModel model = new RamFormModel();       
+                
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(RamFormModel model)
+        {
+            bool isProvider = await this.providerService
+                .ProviderExistsByUserIdAsync(this.User.GetId()!);
+
+            if (!isProvider)
+            {
+                this.TempData[ErrorMessage] = UserCannotAddRamsErrorMessage;
+
+                return this.RedirectToAction("BecomeProvider", "Provider");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            try
+            {
+                string? providerId = await this.providerService
+                .GetProviderByUserIdAsync(this.User.GetId()!);
+
+                int ramId = await this.ramService.CreateRamAsync(providerId!, model);
+
+                this.TempData[SuccessMessage] = RamAddedSuccessfully;
+
+                return this.RedirectToAction("Details", "Ram");
+            }
+            catch (Exception)
+            {
+                this.ModelState.AddModelError(string.Empty, GeneralErrorMessage);
+
+                return this.View(model);
+            }
         }
     }
 }
