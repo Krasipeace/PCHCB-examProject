06029b064f0c7af3c9eb6166ba1aa34de5c65ab0
Krasimir Dramaliev <krasipeace@abv.bg>
2023-07-23 11:54:48 +0300

    moving updateForm js script to new file for code quality

---
+++ PCHCB/PCHCB.Web/wwwroot/js/chooseCooler.js
@@ -0,0 +1,10 @@
+﻿function updateForm() {
+    var currentValue = parseInt($('#Type').val());
+    if (currentValue === 0) {
+        $('#airForm').removeClass('d-none');
+        $('#waterForm').addClass('d-none');
+    } else {
+        $('#airForm').addClass('d-none');
+        $('#waterForm').removeClass('d-none');
+    }
+}
