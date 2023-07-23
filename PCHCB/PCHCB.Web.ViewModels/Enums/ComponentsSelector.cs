dd4b5e43655b6c570876baed4b2017f758d8c9d0
Krasimir Dramaliev <krasipeace@abv.bg>
2023-07-24 02:43:18 +0300

    Create ComponentsSelector.cs

---
+++ PCHCB/PCHCB.Web.ViewModels/Enums/ComponentsSelector.cs
@@ -0,0 +1,14 @@
+﻿namespace PCHCB.Web.ViewModels.Enums
+{
+    public enum ComponentsSelector
+    {
+        Case = 1,
+        Cooler = 2,
+        CPU = 3,
+        GPU = 4,
+        Motherboard = 5,
+        PowerSupply = 6,
+        RAM = 7,
+        Storage = 8
+    }
+}
